e19063e816de2acd0bd909ed4fd6b0fbb6514534
uyoon alam <you@example.com>
2024-04-25 12:54:24 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Governor-AI-Initaitve-Project-Word-Counter 
---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+async function countWords() {
+    let ans = await inquirer.prompt({
+        type: "input",
+        name: "words",
+        message: "Please enter a scentence"
+    });
+    let word_trim = ans.words.trim();
+    let word_split = word_trim.split(" ");
+    let word_count = word_split.length;
+    console.log("There are " + word_count + " words in your scentence");
+}
+countWords();
